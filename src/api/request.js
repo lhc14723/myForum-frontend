845a9e9f97bba295d2c8b7b072e5089d8f3b5c48
lhc14723 <lhckkll@163.com>
2026-02-25 14:27:20 +0800

    add dist

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',  // Django 地址
+  baseURL: '/api',  // Django 地址
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
